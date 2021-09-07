7e45cf8432a9fb03bba048e105882befb1320f57
Carlooss <85409243+carlossdev-svg@users.noreply.github.com>
2021-09-07 18:27:28 +0200

    Fixed problems with points

--- Client/Client.lua
+++ Client/Client.lua
@@ -1,17 +1,14 @@
 ESX = nil
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
+        Wait(0)
     end
 
-    ESX.PlayerData = nil
-    
-    while ESX.PlayerData == nil do
-        ESX.PlayerData = ESX.GetPlayerData()
-        Citizen.Wait(0)
-    end
+    Wait(1000)
+
+    ESX.PlayerData = ESX.GetPlayerData()
 end)
 
 
@@ -23,93 +20,96 @@ end)
 -- Main loop --
 
 CreateThread(function()
-    Wait(1000)
+    Wait(2000)
     while true do
         Wait(0)
 
         local _sleep = true
 
-        for k,v in pairs(Config.Gangs) do
-            if ESX.PlayerData.job.name == v.Job then
-            
-                local _coords = GetEntityCoords(PlayerPedId())
-                local _dist = #(_coords - v.Armory) < 3
-                local _pid = PlayerPedId()
+        if ESX.PlayerData ~= nil then
+            for k,v in pairs(Config.Gangs) do
+                if ESX.PlayerData.job.name == v.Job then
+                
+                    local _coords = GetEntityCoords(PlayerPedId())
+                    local _dist = #(_coords - v.Armory) < 3
+                    local _pid = PlayerPedId()
 
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the armory', v.Armory)
-                    if IsControlJustPressed(0, 38) then
-                        OpenArmoryMenu()
-                    end
-                end
-
-                local _dist = #(_coords - v.SpawnCars) < 3
-
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the garage', v.SpawnCars)
-                    if IsControlJustPressed(0, 38) then
-                        OpenGarageMenu()
-                    end
-                end
-
-                local _dist = #(_coords - v.DeleteCars) < 3
-
-                if _dist then
-                    _sleep = false
-                    if IsPedInAnyVehicle(PlayerPedId()) then
-                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to delete the vehicle', v.DeleteCars)
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the armory', v.Armory)
                         if IsControlJustPressed(0, 38) then
-                            local veh = ESX.Game.GetClosestVehicle(GetEntityCoords(PlayerPedId()))
-                            TaskLeaveVehicle(PlayerPedId(), veh, 0)
-                            Wait(3000)
-                            NetworkFadeOutEntity(veh, false, true)
-                            Wait(1000)
-                            DeleteVehicle(GetVehiclePedIsIn(_pid))
+                            OpenArmoryMenu()
                         end
                     end
-                end
 
-                local _dist = #(_coords - v.Boss) < 3
+                    local _dist = #(_coords - v.SpawnCars) < 3
 
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to access to the boss actions', v.Boss)
-                    if IsControlJustPressed(0, 38) then
-                        TriggerEvent(Config.Triggers.OpenBossMenu, v.Society_Name, function(data, menu)
-                            menu.close()
-                          end, {wash = v.WashMoney})
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the garage', v.SpawnCars)
+                        if IsControlJustPressed(0, 38) then
+                            OpenGarageMenu()
+                        end
                     end
-                end
 
-                local _dist = #(_coords - v.Cloakrooms) < 3
+                    local _dist = #(_coords - v.DeleteCars) < 3
 
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the cloakroom', v.Cloakrooms)
-                    if IsControlJustPressed(0, 38) then
-                        OpenClothesMenu()
+                    if _dist then
+                        _sleep = false
+                        if IsPedInAnyVehicle(PlayerPedId()) then
+                            ShowFloatingHelpNotification('Press ~r~[E]~w~ to delete the vehicle', v.DeleteCars)
+                            if IsControlJustPressed(0, 38) then
+                                local veh = ESX.Game.GetClosestVehicle(GetEntityCoords(PlayerPedId()))
+                                TaskLeaveVehicle(PlayerPedId(), veh, 0)
+                                Wait(3000)
+                                NetworkFadeOutEntity(veh, false, true)
+                                Wait(1000)
+                                DeleteVehicle(GetVehiclePedIsIn(_pid))
+                                ESX.ShowNotification('You have ~r~saved~w~ a ~g~vehicle~s~ in the ~g~garage')
+                            end
+                        end
                     end
-                end
 
-                local _dist = #(_coords - v.Shop) < 3
+                    local _dist = #(_coords - v.Boss) < 3
 
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the gang shop', v.Shop)
-                    if IsControlJustPressed(0, 38) then
-                        OpenShopMenu()
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to access to the boss actions', v.Boss)
+                        if IsControlJustPressed(0, 38) then
+                            TriggerEvent(Config.Triggers.OpenBossMenu, v.Society_Name, function(data, menu)
+                                menu.close()
+                            end, {wash = v.WashMoney})
+                        end
                     end
-                end
 
-                local _dist = #(_coords - v.GangInfo) < 3
+                    local _dist = #(_coords - v.Cloakrooms) < 3
 
-                if _dist then
-                    _sleep = false
-                    ShowFloatingHelpNotification('Press ~r~[E]~w~ to view the personal gang info', v.GangInfo)
-                    if IsControlJustPressed(0, 38) then
-                        OpenGangInfo()
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the cloakroom', v.Cloakrooms)
+                        if IsControlJustPressed(0, 38) then
+                            OpenClothesMenu()
+                        end
+                    end
+
+                    local _dist = #(_coords - v.Shop) < 3
+
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to access the gang shop', v.Shop)
+                        if IsControlJustPressed(0, 38) then
+                            OpenShopMenu()
+                        end
+                    end
+
+                    local _dist = #(_coords - v.GangInfo) < 3
+
+                    if _dist then
+                        _sleep = false
+                        ShowFloatingHelpNotification('Press ~r~[E]~w~ to view the personal gang info', v.GangInfo)
+                        if IsControlJustPressed(0, 38) then
+                            OpenGangInfo()
+                        end
                     end
                 end
             end
