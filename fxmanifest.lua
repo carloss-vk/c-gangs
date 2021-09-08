8df67e87af847230496c73435dc4a6eb78d448ee
Carlooss <85409243+carlossdev-svg@users.noreply.github.com>
2021-09-08 11:47:00 +0200

    Added dependencies

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,7 +15,11 @@ client_scripts {
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
-    '@es_extended/locale.lua',
+    	'@es_extended/locale.lua',
 	"Config/*.lua",
 	"Server/*.lua"
+}
+
+dependency {
+	'fivem-appearance'
 }
