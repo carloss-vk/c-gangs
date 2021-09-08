ec48304fa131258bc13d818ccfe010fb73a5f976
Carlooss <85409243+carlossdev-svg@users.noreply.github.com>
2021-09-08 11:52:54 +0200

    Added check version

--- Server/Server.lua
+++ Server/Server.lua
@@ -16,3 +16,22 @@ AddEventHandler('c-gangs:getItem', function(source, item)
 
     xPlayer.addInventoryItem(item, 1)
 end)
+
+-- VERSION CHECKER DON'T DELETE THIS IF YOU WANT TO RECEIVE NEW UPDATES
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        function checkVersion(error, latestVersion, headers)
+			local currentVersion = Config['Version']    
+            local name = "[^4c-gangs^7]"
+            Citizen.Wait(2000)
+            
+			if tonumber(currentVersion) < tonumber(latestVersion) then
+				print(name .. " ^1is outdated.\nCurrent version: ^8" .. currentVersion .. "\nNewest version: ^2" .. latestVersion .. "\n^3Update^7: https://github.com/carlossdev-svg/c-gangs")
+			else
+				print(name .. " is updated.")
+			end
+		end
+	
+		PerformHttpRequest("https://raw.githubusercontent.com/carlossdev-svg/gangsCheck/main/version", checkVersion, "GET")
+    end
+end)
