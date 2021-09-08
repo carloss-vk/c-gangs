213da47f2acf4feeb0cf53db50dc6f46fe87581d
Carlooss <85409243+carlossdev-svg@users.noreply.github.com>
2021-09-08 11:41:31 +0200

    Changed the function of the clotheshop

--- Client/Modules/Functions.lua
+++ Client/Modules/Functions.lua
@@ -31,6 +31,7 @@ OpenGarageMenu = function(model)
         ESX.Game.SpawnVehicle(data.current.model, v.SpawnVehicle, v.Heading, function(veh)
             SetVehicleCustomPrimaryColour(veh, v.VehicleSpawnColor.r, v.VehicleSpawnColor.g, v.VehicleSpawnColor.b)
             TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
+            ESX.ShowNotification('You have taken out a ~g~'..data.current.model..'~s~ of the ~g~garage')
         end)
     end, function(data, menu)
         menu.close()
@@ -85,127 +86,35 @@ OpenGangInfo = function(source)
 end
 
 OpenClothesMenu = function()
-    local elements = {
-        { label = 'Tu ropa', value = 'default' },
-        { label = 'Conjuntos Guardados', value = 'saved' },
-        { label = 'Comprar ropa', value = 'buy' }
-    }
     ESX.UI.Menu.CloseAll()
-    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'Clothes'..'job-', {
-        title = 'Ropa',
-        align = Config.Align,
-        elements = elements
-    }, function(data, menu)
-        if data.current.value == 'default' then
-            ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin)
-                TriggerEvent(Config.Triggers.LoadSkin, skin)
-            end)
-        end
-        if data.current.value == 'saved' then
-            ESX.TriggerServerCallback('esx_eden_clotheshop:getPlayerDressing', function(dressing)
-                local elements = {}      
-                for i=1, #dressing, 1 do
-                  table.insert(elements, {label = dressing[i], value = i})
-                end
-                ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'player_dressing', {
-                    title    = ('Conjuntos'),
-                    align    = 'bottom-right',
-                    elements = elements,
-                }, function(data, menu)
-                    TriggerEvent(Config.Triggers.GetPSkin, function(skin)
-                        ESX.TriggerServerCallback(Config.Triggers.PlayerOutfit, function(clothes)
-                            TriggerEvent('skinchanger:loadClothes', skin, clothes)
-                            TriggerEvent('esx_skin:setLastSkin', skin)
-                            TriggerEvent('skinchanger:getSkin', function(skin)
-                            TriggerServerEvent('esx_skin:save', skin) end)
-                            ESX.ShowNotification(('Te pusiste el outfit'))
-                        end, data.current.value)
-                    end)
-                end,function(data, menu)
-                    menu.close()
-                end)
-            end)
-        end
-        if data.current.value == "buy" then
-            HasPayed = false
-            TriggerEvent("esx_skin:openRestrictedMenu",function(data, menu)
-                menu.close()
-                ESX.UI.Menu.Open("default",GetCurrentResourceName(),"shop_confirm",
-                    {
-                        title = "¿Quieres comprar la ropa?",
-                        align = "bottom-right",
-                        elements = {
-                            {label = "Si", value = "yes"},
-                            {label = "No", value = "no"}
-                        }
-                    },
-                    function(data, menu)
-                        menu.close()
-                        if data.current.value == "yes" then
-                            ESX.TriggerServerCallback(
-                                "esx_eden_clotheshop:checkMoney",
-                                function(hasEnoughMoney)
-                                    if hasEnoughMoney then
-                                        TriggerEvent("skinchanger:getSkin",function(skin)TriggerServerEvent("esx_skin:save", skin)end)
-                                        TriggerServerEvent("esx_eden_clotheshop:pay")
-                                        ESX.TriggerServerCallback("esx_eden_clotheshop:checkPropertyDataStore",function(foundStore)
-                                            if foundStore then
-                                                ESX.UI.Menu.Open("default",GetCurrentResourceName(),"save_dressing",
-                                                {
-                                                    title = "¿Quieres guardar el conjunto?",
-                                                    align = "bottom-right",
-                                                    elements = {
-                                                        {label = "Si", value = "yes"},
-                                                        {label = "No", value = "no"}
-                                                    }
-                                                },
-                                                function(data2, menu2)
-                                                    menu2.close()
 
-                                                    if data2.current.value == "yes" then
-                                                        ESX.UI.Menu.Open("dialog",GetCurrentResourceName(),"outfit_name",
-                                                        {
-                                                            title = "Nombre del outfit"
-                                                        },
-                                                        function(data3, menu3)
-                                                            menu3.close()
-                                                            TriggerEvent("skinchanger:getSkin",function(skin)
-                                                                TriggerServerEvent("esx_eden_clotheshop:saveOutfit",data3.value,skin)
-                                                            end)
-                                                            ESX.ShowNotification("Guardaste el outfit correctamente")
-                                                        end,
-                                                        function(data3, menu3)
-                                                            menu3.close()
-                                                        end)
-                                                    end
-                                                end)
-                                            end
-                                        end)
-                                    else
-                                        ESX.TriggerServerCallback("esx_skin:getPlayerSkin",function(skin)
-                                            TriggerEvent("skinchanger:loadSkin", skin)
-                                        end)
-                                        ESX.ShowNotification("No tienes suficiente dinero")
-                                    end
-                                end
-                            )
-                        end
-                        if data.current.value == "no" then
-                            ESX.TriggerServerCallback("esx_skin:getPlayerSkin",function(skin)
-                                TriggerEvent("skinchanger:loadSkin", skin)
-                            end)
-                        end
-                    end,
-                    function(data, menu)
-                        menu.close()
-                    end
-                )
-            end,
-            function(data, menu)
-                menu.close()
-            end,{"tshirt_1","tshirt_2","torso_1","torso_2","decals_1","decals_2","arms","arms_2","pants_1","pants_2","shoes_1","shoes_2","chain_1","chain_2","bproof_1","bproof_2","watches_1","watches_2","bracelets_1","bracelets_2",'helmet_1','helmet_2','glasses_1','glasses_2',"bags_1","bags_2"})
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'clothes', {
+        title = 'Skin',
+        align = 'right',
+        elements = {
+            {label = 'Ponerse ropa', value  = 'skin'}
+        }
+    }, function(data, menu)
+
+        if data.current.value == 'skin' then
+            local config = {
+                ped = true,
+                headBlend = true,
+                faceFeatures = true,
+                headOverlays = true,
+                components = true,
+                props = true,
+            }
+            
+            exports['fivem-appearance']:startPlayerCustomization(function (appearance)
+                if (appearance) then
+                print('Saved')
+                else
+                print('Canceled')
+                end
+            end, config)
         end
-    end,function(data, menu)
+    end, function(data, menu)
         menu.close()
     end)
 end
